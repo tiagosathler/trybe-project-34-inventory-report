1c93b0a59d7cfcef75c6d3d3c31e08f8409f52f9
Tiago <sathler@gmail.com>
2022-07-14 09:57:35 -0300

    :test_tube: tests[test_cria_produto.py]: adiciona testes para a classe 'Product'

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from datetime import date
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        23,
+        "Xablau",
+        "Interesting Inc.",
+        date(2020, 11, 13),
+        date(2022, 7, 15),
+        "x003",
+        "Ipsulumloremem",
+    )
+    assert product.id == 23
+    assert product.nome_do_produto == "Xablau"
+    assert product.nome_da_empresa == "Interesting Inc."
+    assert product.data_de_fabricacao == "2020-11-13"
+    assert product.data_de_validade == "2022-07-15"
+    assert product.numero_de_serie == "x003"
+    assert product.instrucoes_de_armazenamento == "Ipsulumloremem"
