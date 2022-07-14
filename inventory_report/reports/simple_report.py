9a6997f4c78376aec5b282f8deee65276b548534
Tiago <sathler@gmail.com>
2022-07-14 16:46:03 -0300

    :recycle: refactor[simple_report.py]: modifica a classe 'SimpleReport' movendo as funções como métodos de classe

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,28 +2,28 @@ from datetime import datetime
 import math
 
 
-def get_diff_date(product: dict) -> int:
-    now = datetime.now()
-    this_date = datetime.fromisoformat(product["data_de_validade"])
-    diff = this_date - now
-    if diff.days < 0:
-        return math.inf
-    return diff.days
-
-
-def get_most_counted_company(products: list[dict]) -> str:
-    companies = [product["nome_da_empresa"] for product in products]
-    most_counted_company = ""
-    max_count = 0
-    for company in companies:
-        current_company_count = companies.count(company)
-        if current_company_count > max_count:
-            most_counted_company = company
-            max_count = current_company_count
-    return most_counted_company
-
-
 class SimpleReport:
+    @classmethod
+    def __get_diff_date(self, product: dict) -> int:
+        now = datetime.now()
+        this_date = datetime.fromisoformat(product["data_de_validade"])
+        diff = this_date - now
+        if diff.days < 0:
+            return math.inf
+        return diff.days
+
+    @classmethod
+    def __get_most_counted_company(self, products: list[dict]) -> str:
+        companies = [product["nome_da_empresa"] for product in products]
+        most_counted_company = ""
+        max_count = 0
+        for company in companies:
+            current_company_count = companies.count(company)
+            if current_company_count > max_count:
+                most_counted_company = company
+                max_count = current_company_count
+        return most_counted_company
+
     @classmethod
     def generate(self, products: list[dict]) -> str:
         """
@@ -36,10 +36,12 @@ class SimpleReport:
         products.sort(key=lambda product: product["data_de_fabricacao"])
         oldest_product = products[0].get("data_de_fabricacao")
 
-        products.sort(key=get_diff_date)
+        products.sort(key=SimpleReport.__get_diff_date)
         closest_validity_product = products[0].get("data_de_validade")
 
-        most_counted_company = get_most_counted_company(products)
+        most_counted_company = SimpleReport.__get_most_counted_company(
+            products
+        )
 
         return str(
             f"Data de fabricação mais antiga: {oldest_product}\n"
