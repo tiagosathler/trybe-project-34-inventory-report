6aedc1dc88cb39ef946f8c4984f6838e873aa3f5
Tiago <sathler@gmail.com>
2022-07-28 09:36:43 -0300

    :recycle: refactor[simple_report.py]: refatoração geral da classe SimpleReport, adequando-a a versão 3.8.10 do Python e outras melhorias

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,47 +1,97 @@
 from datetime import datetime
-import math
 
 
 class SimpleReport:
     @classmethod
-    def __get_diff_date(cls, product: dict) -> int:
-        now = datetime.today()
+    def _get_diff_date(cls, product: dict) -> int:
+        """
+        Método desta classe, privado, para ser usado no método 'sort'.
+        Calcula a diferença em segundos da data atual com a data de
+        fabricação de um produto.
+
+        Entrada:
+        --------
+
+        product: dict
+            dicionário produto com a chave 'data_de_validade'
+
+        Saída:
+        ------
+
+        seconds: int
+            a diferença em segundos da 'data_de_validade' com a data atual
+        """
+        now = datetime.now()
         this_date = datetime.fromisoformat(product["data_de_validade"])
         diff = this_date - now
-        if diff.days < 0:
-            return math.inf
-        return diff.days
+        return diff.seconds
 
     @classmethod
-    def __get_most_counted_company(cls, products: list[dict]) -> str:
-        companies = [product["nome_da_empresa"] for product in products]
-        most_counted_company = ""
-        max_count = 0
-        for company in companies:
-            current_company_count = companies.count(company)
-            if current_company_count > max_count:
-                most_counted_company = company
-                max_count = current_company_count
-        return most_counted_company
-
-    @classmethod
-    def generate(cls, products: list[dict]) -> str:
+    def _get_most_counted_company(cls, products: list) -> str:
         """
-        Recebe uma lista de dicionários de produtos e retorna
-        uma string com a 'Data de fabricação mais antiga',
-        a 'Data de validade mais próxima' e
-        o nome da 'Empresa com mais produtos'.
+        Método desta classe, privado, para ser usado pelo método 'generate'
+        da mesma classe.
+        Encontra o nome da empresa mais repetida na lista de dicionários de
+        produtos.
+
+        Entrada:
+        --------
+
+        products: list[dict]
+            lista com os dicionário de cada produto
+
+        Saída:
+        ------
+
+        most_counted_company: str
+            o nome da empresa mais repetida na lista de dicionários de produtos
+        """
+        companies = [product["nome_da_empresa"] for product in products]
+        companies_list = dict.fromkeys(companies, 0)
+
+        for company_name in companies_list.keys():
+            companies_list[company_name] = companies.count(company_name)
+
+        companies_list = [
+            {"name": k, "count": v} for (k, v) in companies_list.items()
+        ]
+
+        companies_list.sort(
+            key=lambda company: company["count"],
+            reverse=True,
+        )
+
+        return companies_list[0]["name"]
+
+    @classmethod
+    def generate(cls, products: list) -> str:
+        """
+        Método público da classe SimpleReport para gerar um relatório simples.
+
+        Entrada:
+        --------
+
+        products: list[dict]
+            lista com os dicionário de cada produto
+
+        Saída:
+        ------
+
+        relatório: str
+            Data de fabricação mais antiga: YYYY-MM-DD
+            Data de validade mais próxima: YYYY-MM-DD
+            Empresa com mais produtos: NOME_DA_EMPRESA
         """
 
         products.sort(key=lambda product: product["data_de_fabricacao"])
+
         oldest_product = products[0].get("data_de_fabricacao")
 
-        products.sort(key=SimpleReport.__get_diff_date)
+        products.sort(key=SimpleReport._get_diff_date)
+
         closest_validity_product = products[0].get("data_de_validade")
 
-        most_counted_company = SimpleReport.__get_most_counted_company(
-            products
-        )
+        most_counted_company = SimpleReport._get_most_counted_company(products)
 
         return str(
             f"Data de fabricação mais antiga: {oldest_product}\n"
